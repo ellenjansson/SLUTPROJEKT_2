d7ceb9298b5a7074d8795a0dedeff0b8667051f0
ellenjansson <ellen04jansson@gmail.com>
2022-05-09 20:40:55 +0200

    Fixed too long/short answers and a restart option

--- wordle.py
+++ wordle.py
@@ -1,12 +1,23 @@
+from pdb import Restart
 import random
+import os
+from time import sleep
 from collections import Counter
+from colorama import init
+init()
 
 BOX = "▉"
 GREEN = "\033[1;32;40m"
 YELLOW = "\033[1;33;40m"
 WHITE = "\033[1;37;40m"
 
-class word:
+# TODO:
+# - Fixa docstring
+# - Fixa korrekt namngivning - 
+# - Fixa längd på gissning (Inte mer eller mindre än 5) -
+# - Låt användaren avsluta spelet (rensa konsollen innan nästa spel?) -
+
+class Word:
 
     #En funktion som öppnar en text fil och gör den till en variabel self.local_word som kan användas i koden. 
     def __init__(self):
@@ -15,7 +26,7 @@ class word:
         f.close()
         self.local_word = random.choice(words)
         print (self.local_word)
-    
+            
     #Funktion med en guess variabel som används till att korrigera om det gissade ordet är korrekt eller inte. 
     def compare(self, guess):
         """_summary_
@@ -26,14 +37,16 @@ class word:
         Returns:
             bool: True if correct guess, else False
         """
+    
         self.guess = guess
         
         #En if-sats som kollar om det gissade ordet är lika med self.local_word (det slumpmässiga ordet)
         #Om self.local_word är lika med det gissade ordet skrivs det ut att man har rätt och 5 färgade rutor blir färgade grönt.
         if guess == self.local_word:
             print("\nCorrect!") 
-            print(GREEN.encode('utf-8'), BOX * 5)
+            print(GREEN, BOX * 5)
             return True
+        
         #Om self.local_word inte matchar det gissade ordet skrivs det ut att man har fel.
         else:
             print("Wrong!")
@@ -45,10 +58,8 @@ class word:
                 if guess[i] == self.local_word[i]:
                     color_matrix.append(GREEN + BOX)
                     chars[guess[i]] -= 1
-                #Om bokstaven från det gissade ordet finns i self.local_word fast inte är på rätt plats, blir den motsvarande rutan till den bokstaven gul.
                 elif guess[i] in self.local_word:
                     color_matrix.append(YELLOW + BOX)
-                #Om bokstaven från det gissade ordet inte finns i self.local_word blir motsvarande rutan till ordet grått. 
                 else:
                     color_matrix.append(WHITE + BOX)
             #Här kollar programmet 
@@ -61,26 +72,51 @@ class word:
                 print(color, end = " ")
             print()
         return False
+            
     
-class game:
+class Game:
     
     def __init__(self):
         self.guesses = 0
         self.max_guesses = 6
         
     def play_game(self):
-        my_word = word()
+        my_word = Word()
         
         while self.guesses<self.max_guesses:
-            self.guesses = self.guesses + 1
-            print("Guesses:", self.guesses)
-            if my_word.compare(input("Enter a word: ")):
-                break
-        
-def main():
-    new_game = game()
-    new_game.play_game()
+            self.tmp_guess = input("Enter a word: ")
 
+            if (len(self.tmp_guess)<5):
+                print("Too short word!")
+                
+            elif (len(self.tmp_guess)>5):        
+                print("Too long word!")
+
+            else: 
+                self.guesses = self.guesses + 1
+                print("Guesses:", self.guesses)
+                if my_word.compare(self.tmp_guess):
+                    break
+            
+        restart = input("Do you want to restart the game? (y/n)")
+        
+        if restart == "y":
+            print("The game will now restart")
+            sleep(2)
+            os.system('cls')
+            self.__init__()
+            self.play_game()
+        elif restart == "n":
+            os.system('cls')
+            print("You have chosen to exit the game, thank you for playing!")
+            exit()
+        else:
+            print("Please write y or n")
+            Game()
+            
+def main():
+    new_game = Game()
+    new_game.play_game()
 
 if __name__ == "__main__":
     main()
